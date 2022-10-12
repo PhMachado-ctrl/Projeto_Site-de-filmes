41520d6d186559f0c91c0f2bead9d8c5a7cc3fce
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-10-12 19:44:05 -0300

    cadastrar/galeria.html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Projeto_Site-de-filmes" 
+
+
--- galeria.php
+++ galeria.php
@@ -18,9 +18,8 @@
       <a href="#" class="brand-logo">Logo</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="badges.html">Cadastrar</a></li>
       </ul>
     </div>
     <div class="nav-content">
