4b81ffc2e5c1ba0c3169c87e68ea21bf7f0565b9
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-04 08:47:41 -0300

    Card title

--- galeria.php
+++ galeria.php
@@ -40,8 +40,8 @@
       <div class="card">
         <div class="card-image">
           <img src="http://materializecss.com/images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite-border</i></a>
+          <span class="card-title">Vingadores</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
         </div>
         <div class="card-content">
           <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
