97f4b1f59585d42f254c28020b4346575337fe6b
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-04 08:23:47 -0300

    Colunas-row

--- galeria.php
+++ galeria.php
@@ -32,14 +32,20 @@
     </div>
   </nav>
 
-  <div class="card">
-    <div class="card-image">
-      <img src="http://materializecss.com/images/sample-1.jpg">
-      <span class="card-title">Card Title</span>
-      <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-    </div>
-    <div class="card-content">
-      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+
+
+  <div class="row">
+    <div class="col s3">
+      <div class="card">
+        <div class="card-image">
+          <img src="http://materializecss.com/images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
     </div>
   </div>
     
