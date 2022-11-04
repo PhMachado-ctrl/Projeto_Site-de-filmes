5574aa4c2a107b2079b29ff95fc472bd1943e7cc
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-04 09:50:49 -0300

    Title cards position

--- galeria.php
+++ galeria.php
@@ -40,18 +40,38 @@
       <div class="card hoverable">
         <div class="card-image">
           <img src="https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
-          <span class="card-title">Vingadores</span>
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
         </div>
         <div class="card-content">
           <p class="valign-wrapper">
             <i class="material-icons amber-text">star</i>  9,7
           </p>
+          <span class="card-title">Vingadores</span>
+          
+          <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card hoverable">
+        <div class="card-image">
+          <img src="https://www.themoviedb.org/t/p/w300/4aLjHD7sfMzEXin4qa7v6AJYo9R.jpg">
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">favorite_border</i></a>
+        </div>
+        <div class="card-content">
+          <p class="valign-wrapper">
+            <i class="material-icons amber-text">star</i>  9,7
+          </p>
+          <span class="card-title">Top Gun: Maverick</span>
+
           <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
         </div>
       </div>
     </div>
   </div>
+
+  
     
 </body>
 </html>
