48bd49f511f0d4e410ac2b379d9dfe1adf34e629
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-05 07:47:55 -0300

    cancelar

--- cadastrar.php
+++ cadastrar.php
@@ -37,11 +37,11 @@
             <div class="col s6 offset-s3">
                 <div class="card ">
                     <div class="card-content">
-                        <span class="card-title"></span>
+                        <span class="card-title">Cadastrar Filme</span>
                         <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
                     </div>
                     <div class="card-action">
-                        <a href="#">This is a link</a>
+                        <a href="galeria.php">Cancelar</a>
                         <a href="#">This is a link</a>
                     </div>
                 </div>
