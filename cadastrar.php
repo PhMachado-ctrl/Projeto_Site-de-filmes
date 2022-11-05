8bd7dc3f87548699887d09768bfc7ea5cc00d2e2
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-05 06:43:20 -0300

    card cadastrar

--- cadastrar.php
+++ cadastrar.php
@@ -1,12 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <li class="tab"><a href="#test2">Assistidos</a></li>
+    <li class="tab disabled"><a href="#test3">Favoritos</a></li>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crorocine</title>
+    </head>
+    <body>
+        <nav class="nav-extended purple lighten-3">
+            <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="badges.html">Cadastrar</a></li>
+            </ul>
+            </div>
+            <div class="nav-header center">
+            <h1>Crorocine</h1>
+            </div>
+            <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+            </div>
+        </nav>
+
+        <div class="row">
+            <div class="col s6">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title"></span>
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     
-</body>
+    </body>
 </html>
