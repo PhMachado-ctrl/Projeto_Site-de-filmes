3d4f824ae73c264ddaadbeef7ccd7264415aff98
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-05 06:54:32 -0300

    card text black

--- cadastrar.php
+++ cadastrar.php
@@ -36,7 +36,7 @@
         <div class="row">
             <div class="col s6 offset-s3">
                 <div class="card ">
-                    <div class="card-content white-text">
+                    <div class="card-content">
                         <span class="card-title"></span>
                         <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
                     </div>
