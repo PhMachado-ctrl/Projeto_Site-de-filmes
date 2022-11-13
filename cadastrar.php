6b5440247181ea552356fc4dd9936c1a9c98df89
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-11-13 11:38:26 -0300

    sinopse

--- cadastrar.php
+++ cadastrar.php
@@ -17,19 +17,19 @@
     <body>
         <nav class="nav-extended purple lighten-3">
             <div class="nav-wrapper">
-            <ul id="nav-mobile" class="right">
-                <li><a href="galeria.php">Galeria</a></li>
-                <li><a href="badges.html">Cadastrar</a></li>
-            </ul>
+                <ul id="nav-mobile" class="right">
+                    <li><a href="galeria.php">Galeria</a></li>
+                    <li><a href="badges.html">Cadastrar</a></li>
+                </ul>
             </div>
             <div class="nav-header center">
-            <h1>Crorocine</h1>
+                <h1>Crorocine</h1>
             </div>
             <div class="nav-content">
-            <ul class="tabs tabs-transparent purple darken-1">
-                <li class="tab"><a class="active" href="#test1">Todos</a></li>
-                <li class="tab"><a href="#test2">Assistidos</a></li>
-                <li class="tab disabled"><a href="#test3">Favoritos</a></li>
+                <ul class="tabs tabs-transparent purple darken-1">
+                    <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                    <li class="tab"><a href="#test2">Assistidos</a></li>
+                    <li class="tab disabled"><a href="#test3">Favoritos</a></li>
             </div>
         </nav>
 
@@ -38,13 +38,29 @@
                 <div class="card ">
                     <div class="card-content">
                         <span class="card-title">Cadastrar Filme</span>
-                        <input  id="titulo" type="text" class="validate">
-                        <label for="titulo">Disabled</label>
+                            
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input  id="titulo" type="text" class="validate"require>
+                                <label for="titulo">Titulo do Filme</label>                       
+                            </div>
+                        </div>    
+
                     </div>
-                    <div class="card-action">
-                        <a class="waves-effect waves-light btn grey" href="galeria.php">Cancelar</a>
-                        <a href="#" class="waves-effect waves-light btn purple">Confirmar</a>
-                    </div>
+                <div class="row">
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <textarea id="sinopse" class="materialize-textarea"></textarea>
+                                <label for="sinopse">Sinopse</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="card-action">
+                    <a class="waves-effect waves-light btn grey" href="galeria.php">Cancelar</a>
+                    <a href="#" class="waves-effect waves-light btn purple">Confirmar</a>
                 </div>
             </div>
         </div>
